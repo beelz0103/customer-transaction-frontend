9716416fd2c8f2b2e34b771c333bb987bf764680
Tet <47019626+beelz0103@users.noreply.github.com>
2023-12-03 22:20:30 +0530

    Scaffold app using auth0 library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { AuthenticationGuard } from "./authentication-guard";
+
+const BASE_URL = import.meta.env.VITE_API_SERVER_URL;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/profile"
+          element={<AuthenticationGuard component={ProfilePage} />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+const HomePage = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  console.log(isAuthenticated);
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/profile",
+      },
+      authorizationParams: {
+        prompt: "login",
+      },
+    });
+  };
+
+  return <button onClick={handleLogin}>Log In</button>;
+};
+
+const ProfilePage = () => {
+  const { logout } = useAuth0();
+  const [data, setData] = useState("");
+
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const accessToken = await getAccessTokenSilently();
+
+      const response = await fetch(`${BASE_URL}/auth/protected`, {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const data = await response.json();
+
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+  return (
+    <div>
+      <div>{data}</div>
+      <button onClick={handleLogout}>Log Out</button>
+    </div>
+  );
+};
+
+export default App;
